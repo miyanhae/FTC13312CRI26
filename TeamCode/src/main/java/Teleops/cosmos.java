f023eb7589c44e93ee1f75e60db9fff33c87e8bb
Mia <givemiacupoftea@gmail.com>
2026-06-13 14:53:59 -0500

    limelight + drive stuff

--- TeamCode/src/main/java/Teleops/cosmos.java
+++ TeamCode/src/main/java/Teleops/cosmos.java
@@ -1,4 +1,5 @@
 package Teleops;
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -7,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 @TeleOp(name = "cosmos", group = "LinearOpMode")
 public class cosmos extends LinearOpMode
@@ -23,7 +25,7 @@ public class cosmos extends LinearOpMode
     private double driveSensitivity = 1;
 
     @Override
-    public void runOpMode()
+    public void runOpMode() throws InterruptedException
     {
         waitForStart();
         //hardware maps
@@ -44,10 +46,45 @@ public class cosmos extends LinearOpMode
         limelight.pipelineSwitch(0); //0 is blue 1 is red
 
 
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
 
 
 
 
+
+
+        //start polling for data
+        limelight.start();
+
+        while (opModeIsActive())
+        {
+            double drivePower = -gamepad1.left_stick_y;
+            double turnPower = gamepad1.right_stick_x;
+            double strafePower = gamepad1.left_stick_x;
+
+            double lfPower = Range.clip(drivePower + turnPower + strafePower, -driveSensitivity, driveSensitivity);
+            double rfPower = Range.clip(drivePower - turnPower - strafePower, -driveSensitivity, driveSensitivity);
+            double lbPower = Range.clip(drivePower + turnPower - strafePower, -driveSensitivity, driveSensitivity);
+            double rbPower = Range.clip(drivePower - turnPower + strafePower, -driveSensitivity, driveSensitivity);
+
+
+
+
+            //get most recent frame of data caught by camera
+            //this code is literally just from the limelight documentation
+            LLResult result = limelight.getLatestResult();
+            if (result != null)
+            {
+                if(result.isValid())
+                {
+                    Pose3D botpose = result.getBotpose();
+                    telemetry.addData("tx", result.getTx());
+                    telemetry.addData("ty", result.getTy());
+                    telemetry.addData("Botpose", botpose.toString());
+                }
+            }
+        }
     }
 
 }
