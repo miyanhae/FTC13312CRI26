6b61ccc20063f98d70c905b54a95b48e800d4c2f
Mia <givemiacupoftea@gmail.com>
2026-06-13 14:16:28 -0500

    more stuff

--- TeamCode/src/main/java/Teleops/cosmos.java
+++ TeamCode/src/main/java/Teleops/cosmos.java
@@ -1,24 +1,43 @@
 package Teleops;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 @TeleOp(name = "cosmos", group = "LinearOpMode")
 public class cosmos extends LinearOpMode
 {
     //Variables
     private DcMotor leftFront, leftBack, rightFront, rightBack;
-    private DcMotor shooter;
-    private Servo servo1, servo2;
-
+    private DcMotor shooter1, shooter2;
+    private CRServo turret1, turret2;
+    private CRServo hood;
+    private CRServo blocker;
+    private Limelight3A limelight;
 
 
     @Override
     public void runOpMode()
     {
+        //hardware maps
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
+        shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
+
+        turret1 = hardwareMap.get(CRServo.class, "turret1");
+        turret2 = hardwareMap.get(CRServo.class, "turret2");
+        hood = hardwareMap.get(CRServo.class, "hood");
+        blocker = hardwareMap.get(CRServo.class, "blocker");
+
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
     }
 
